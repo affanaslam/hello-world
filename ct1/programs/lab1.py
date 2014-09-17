8800f0112f1ed7ee951fd242713b8a5aa08c56cf
sa00310 <sa00310@04d80683-4801-0010-8521-0b09cf1cc44e>
2014-09-17 11:23:24 +0000

    First commit.

---
+++ ct1/programs/lab1.py
@@ -0,0 +1,28 @@
+import math 
+
+def isPalindrome(s):
+    a = s[::-1].lower() #lowercase
+    b = s[:].lower() #lowercase
+    if a == b:
+        return True
+    else:
+        return False
+
+def isPalindrome1 (s):
+    if len(s) < 2:
+        return True
+    return s[0] == s[-1] and isPalindrome1(s[1:-1])
+
+def binaryString (n):
+
+    b = ""
+    p = int (math.log (n,2))
+    for i in range (p,0,-1):
+        if n >= i:
+            b = b + "1"
+            n - 2**i 
+        elif:
+            b = b + "0"
+    return b
+            
+            
